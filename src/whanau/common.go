9f691c91d2907fff513e49eba6dfa3a6ec9ab98a
qqliu <qliu2011@gmail.com>
2014-04-26 15:40:50 -0400

    fixing merge errors

--- src/whanau/common.go
+++ src/whanau/common.go
@@ -24,7 +24,6 @@ const (
 type Err string
 
 // Global Parameters
-<<<<<<< HEAD
 const (
     W = 1  // mixing time of honest region
     RD = 1 // size of db
@@ -34,12 +33,3 @@ const (
     PaxosSize = 5
     PaxosWalk = 5
 )
-=======
-var W = 1  // mixing time of honest region
-var RD = 1 // size of db
-var RF = 1 // size of fingertable
-var RS = 1 // size of succ records
-var L = 1  // number of layers
-var PaxosSize = 5
-var PaxosWalk = 5
->>>>>>> 9af5fe49ef88b5e123cc673f71f47407f5fa8442

445a2aabf145b4112e3834950d512f6ab5e3a2f6
qqliu <qliu2011@gmail.com>
2014-04-28 21:19:37 -0400

    added methods for sorting fingers and successors

--- src/whanau/server.go
+++ src/whanau/server.go
@@ -202,10 +202,15 @@ func (ws *WhanauServer) Setup(nlayers int, rf int) {
 	for i := 0; i < nlayers; i++ {
 		// populate tables in layers
 		ws.ids = append(ws.ids, ws.ChooseID(i))
-		ws.fingers = append(ws.fingers, ws.ConstructFingers(i, rf))
-		ws.succ = append(ws.succ, ws.Successors(i))
-	}
 
+        curFingerTable := ws.ConstructFingers(i, rf)
+        ByFinger(FingerId).Sort(curFingerTable)
+		ws.fingers = append(ws.fingers, curFingerTable)
+
+        curSuccessorTable := ws.Successors(i)
+        By(RecordKey).Sort(curSuccessorTable)
+		ws.succ = append(ws.succ, curSuccessorTable)
+	}
 }
 
 // return random Key/value record from local storage
@@ -288,7 +293,8 @@ func (ws *WhanauServer) ChooseID(layer int) KeyType {
 }
 
 // Defines ordering of Record args
-type By func(p1, p2 *Record) bool
+type By func(r1, r2 *Record) bool
+type ByFinger func(f1, f2 *Finger) bool
 
 // Sort uses By to sort the Record slice
 func (by By) Sort(records []Record) {
@@ -299,10 +305,25 @@ func (by By) Sort(records []Record) {
 	sort.Sort(rs)
 }
 
+// sort uses By to sort fingers by id
+func (by ByFinger) Sort(fingers []Finger) {
+    fs := &fingerSorter{
+        fingers: fingers,
+        by:      by,
+    }
+    sort.Sort(fs)
+}
+
 // recordSorter joins a By function and a slice of Records to be sorted.
 type recordSorter struct {
 	records []Record
-	by      func(p1, p2 *Record) bool // Closure used in the Less method.
+	by      func(r1, r2 *Record) bool // Closure used in the Less method.
+}
+
+// fingerSorter joins a By function and a slice of Fingers to be sorted.
+type fingerSorter struct {
+    fingers []Finger
+    by      func(f1, f2 *Finger) bool // Closure used in the Less method
 }
 
 // Len is part of sort.Interface.
@@ -310,9 +331,17 @@ func (s *recordSorter) Len() int {
 	return len(s.records)
 }
 
+func (s *fingerSorter) Len() int {
+    return len(s.fingers)
+}
+
 // Swap is part of sort.Interface.
 func (s *recordSorter) Swap(i, j int) {
 	s.records[i], s.records[j] = s.records[j], s.records[i]
+}
+
+func (s *fingerSorter) Swap(i, j int) {
+    s.fingers[i], s.fingers[j] = s.fingers[j], s.fingers[i]
 }
 
 // Less is part of sort.Interface. It is implemented by calling the "by" closure in the sorter.
@@ -320,12 +349,21 @@ func (s *recordSorter) Less(i, j int) bool {
 	return s.by(&s.records[i], &s.records[j])
 }
 
+func (s *fingerSorter) Less(i, j int) bool {
+    return s.by(&s.fingers[i], &s.fingers[j])
+}
+
+var RecordKey = func(r1, r2 *Record) bool {
+    return r1.Key < r2.Key
+}
+
+var FingerId = func(f1, f2 *Finger) bool {
+    return f1.Id < f2.Id
+}
+
 // Gets successors that are nearest each key
 func (ws *WhanauServer) SampleSuccessors(args *SampleSuccessorsArgs, reply *SampleSuccessorsReply) error {
-	recordKey := func(r1, r2 *Record) bool {
-		return r1.Key < r2.Key
-	}
-	By(recordKey).Sort(ws.db)
+	By(RecordKey).Sort(ws.db)
 
 	key := args.Key
 	t := args.T

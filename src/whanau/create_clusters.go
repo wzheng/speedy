3c707476d662b4a3801d03ae4a6942b718172cda
Pratiksha Thaker <prthaker@mit.edu>
2014-05-10 22:06:22 -0400

    Reorganizing files again

---
+++ src/whanau/create_clusters.go
@@ -0,0 +1,178 @@
+package whanau
+
+/*
+   Paxos cluster create and join functions
+*/
+
+import "fmt"
+import "math/rand"
+
+// Master node function
+// When a server tells the master node what paxos cluster it's a part
+// of, the master node sends it any pending writes that were assigned
+// to that server so the server can put it in the paxos cluster.
+func (ws *WhanauServer) ReceiveNewPaxosCluster(
+	args *ReceiveNewPaxosClusterArgs,
+	reply *ReceiveNewPaxosClusterReply) error {
+	ws.new_paxos_clusters = append(ws.new_paxos_clusters, args.Cluster)
+
+	// go through the dictionary to see if the master already
+	// has some keys for it
+	send_keys := make(map[KeyType]TrueValueType)
+	fmt.Printf("looking for server %v\n", args.Server)
+	ws.mu.Lock()
+	for k, v := range ws.key_to_server {
+		if v == args.Server {
+			if value, found := ws.all_pending_writes[k]; found {
+				fmt.Printf("found in pending writes %v\n", value)
+				send_keys[k.Key] = value
+				// deletes should be safe
+				delete(ws.key_to_server, k)
+				delete(ws.all_pending_writes, k)
+				fmt.Printf("send keys is now %v\n", send_keys)
+			}
+		}
+	}
+	ws.mu.Unlock()
+
+	reply.KV = send_keys
+	reply.Err = OK
+
+	return nil
+}
+
+// Join the cluster and add the new keys that this cluster is
+// responsible for.
+func (ws *WhanauServer) JoinClusterRPC(args *JoinClusterArgs,
+	reply *JoinClusterReply) error {
+
+	var index int
+	for idx, s := range args.NewCluster {
+		if s == ws.myaddr {
+			index = idx
+		}
+	}
+
+	wp := StartWhanauPaxos(args.NewCluster, index, ws.rpc)
+
+	for k, v := range args.KV {
+		// initiate paxos call for all of these keys
+
+		// put into one's own kvstore
+		ws.mu.Lock()
+		ws.kvstore[k] = ValueType{args.NewCluster}
+		ws.paxosInstances[k] = *wp
+		ws.mu.Unlock()
+
+		cpargs := &ClientPutArgs{k, v, NRand(), ws.myaddr}
+		cpreply := &ClientPutReply{}
+		ws.PaxosPutRPC(cpargs, cpreply)
+
+	}
+
+	reply.Err = OK
+	return nil
+}
+
+func (ws *WhanauServer) InitPaxosCluster(args *InitPaxosClusterArgs, reply *InitPaxosClusterReply) error {
+	if args.Phase == PhaseOne {
+		reply.Reply = Commit
+	} else {
+		if args.Action == Commit {
+			// first, find the index of itself in the list of servers
+			servers := args.Servers
+			var index int
+			for idx, s := range servers {
+				if s == ws.myaddr {
+					index = idx
+				}
+			}
+
+			for k, v := range args.KeyMap {
+				ws.mu.Lock()
+				ws.kvstore[k] = ValueType{servers}
+				fmt.Printf("\ninitiating wp in server %v ... \n\n", ws.me)
+				wp := StartWhanauPaxos(servers, index, ws.rpc)
+				ws.paxosInstances[k] = *wp
+				ws.paxosInstances[k].db[k] = v
+				ws.mu.Unlock()
+			}
+		} else {
+			// do nothing?
+		}
+	}
+
+	return nil
+}
+
+func (ws *WhanauServer) ConstructPaxosCluster() []string {
+
+	cluster := make(map[string]bool)
+
+	randIndex := rand.Intn(len(ws.neighbors))
+	neighbor := ws.neighbors[randIndex]
+
+	// pick several random walk nodes to join the Paxos cluster
+	for i := 0; i < PaxosSize-1; i++ {
+		args := &RandomWalkArgs{}
+		args.Steps = PaxosWalk
+		var reply RandomWalkReply
+		ok := call(neighbor, "WhanauServer.RandomWalk", args, &reply)
+		if ok && (reply.Err == OK) {
+
+			if _, found := cluster[reply.Server]; found || reply.Server == ws.myaddr {
+				i--
+				continue
+			}
+
+			cluster[reply.Server] = true
+		}
+	}
+
+	// initiate 2PC with all the nodes in the tentative paxos cluster
+	// pass key-value information in the second phase, if it's okay to commit
+
+	var if_commit = true
+
+	for c, _ := range cluster {
+		args := &InitPaxosClusterArgs{}
+		var reply InitPaxosClusterReply
+		args.RequestServer = ws.myaddr
+		args.Phase = PhaseOne
+		args.Action = ""
+		ok := call(c, "WhanauServer.InitPaxosCluster", args, &reply)
+		if ok && (reply.Err == OK) {
+			if reply.Reply == Reject {
+				if_commit = false
+				break
+			}
+		}
+	}
+
+	// Send commit message to every server, along with the key
+	for c, _ := range cluster {
+		args := &InitPaxosClusterArgs{}
+		var reply InitPaxosClusterReply
+		args.RequestServer = ws.myaddr
+		args.Phase = PhaseTwo
+
+		if if_commit {
+			args.Action = Commit
+		} else {
+			args.Action = Abort
+		}
+
+		ok := call(c, "WhanauServer.InitPaxosCluster", args, &reply)
+		if ok && (reply.Err == OK) {
+		}
+	}
+
+	var ret []string
+	for c, _ := range cluster {
+		ret = append(ret, c)
+	}
+
+	ret = append(ret, ws.myaddr)
+
+	return ret
+}
--- src/whanau/test_test.go
+++ src/whanau/test_test.go
@@ -56,56 +56,6 @@ func testGetId(server string, layer int) KeyType {
 	return "GETID ERR"
 }
 
-/*
-func TestBasic(t *testing.T) {
-	runtime.GOMAXPROCS(4)
-
-	const nservers = 3
-	var ws []*WhanauServer = make([]*WhanauServer, nservers)
-	var kvh []string = make([]string, nservers)
-	defer cleanup(ws)
-
-	for i := 0; i < nservers; i++ {
-		kvh[i] = port("basic", i)
-	}
-
-	for i := 0; i < nservers; i++ {
-		neighbors := make([]string, 0)
-		for j := 0; j < nservers; j++ {
-			if j == i {
-				continue
-			}
-			neighbors = append(neighbors, kvh[j])
-		}
-
-		ws[i] = StartServer(kvh, i, kvh[i], neighbors)
-	}
-
-	var cka [nservers]*Clerk
-	for i := 0; i < nservers; i++ {
-		cka[i] = MakeClerk(kvh[i])
-	}
-
-	fmt.Printf("Test: Basic put/lookup ...\n")
-
-	cka[1].Put("a", "x")
-	val := cka[1].Lookup("a")
-
-	fmt.Printf("lookup for key a got value %s\n", val)
-
-	fmt.Printf("...Passed\n")
-
-	fmt.Printf("Lookup in neighboring server ...\n")
-
-	cka[2].Put("b", "y")
-	val = cka[1].Lookup("b")
-
-	fmt.Printf("lookup for key b got value %s\n", val)
-
-	fmt.Printf("...Passed\n")
-}
-*/
-
 func TestLookup(t *testing.T) {
 	runtime.GOMAXPROCS(8)
 
@@ -140,7 +90,8 @@ func TestLookup(t *testing.T) {
 			neighbors = append(neighbors, kvh[j])
 		}
 
-		ws[i] = StartServer(kvh, i, kvh[i], neighbors, make([]string, 0), false, false,
+		ws[i] = StartServer(kvh, i, kvh[i], neighbors, make([]string, 0),
+			false, false,
 			nlayers, nfingers, w, rd, rs, ts)
 	}
 
@@ -157,7 +108,6 @@ func TestLookup(t *testing.T) {
 	// hard code in records for each server
 	for i := 0; i < nservers; i++ {
 		for j := 0; j < nkeys/nservers; j++ {
-			//var key KeyType = testKeys[counter]
 			var key KeyType = KeyType(strconv.Itoa(counter))
 			keys = append(keys, key)
 			counter++
@@ -301,53 +251,6 @@ func TestLookup(t *testing.T) {
 	fmt.Printf("numFound: %d\n", numFound)
 	fmt.Printf("total keys: %d\n", nkeys)
 	fmt.Printf("Percent lookups successful: %f\n", float64(numFound)/float64(numTotal))
-
-}
-
-// Test a basic put/get using paxos without checking lookup integrity
-func TestPutGet(t *testing.T) {
-	runtime.GOMAXPROCS(4)
-
-	fmt.Printf("\033[95m%s\033[0m\n", "Test: Basic PutGet")
-	const nservers = 10
-	const nkeys = 20           // keys are strings from 0 to 99
-	const k = nkeys / nservers // keys per node
-
-	// run setup in parallel
-	// parameters
-	constant := 5
-	nlayers := constant*int(math.Log(float64(k*nservers))) + 1
-	nfingers := constant * int(math.Sqrt(k*nservers))
-	w := constant * int(math.Log(float64(nservers))) // number of steps in random walks, O(log n) where n = nservers
-	rd := constant * int(math.Sqrt(k*nservers))      // number of records in the db
-	rs := constant * int(math.Sqrt(k*nservers))      // number of nodes to sample to get successors
-	ts := constant                                   // number of successors sampled per node
-
-	var ws []*WhanauServer = make([]*WhanauServer, nservers)
-	var kvh []string = make([]string, nservers)
-	defer cleanup(ws)
-
-	for i := 0; i < nservers; i++ {
-		kvh[i] = port("basic", i)
-	}
-
-	for i := 0; i < nservers; i++ {
-		neighbors := make([]string, 0)
-		for j := 0; j < nservers; j++ {
-			if j == i {
-				continue
-			}
-			neighbors = append(neighbors, kvh[j])
-		}
-
-		ws[i] = StartServer(kvh, i, kvh[i], neighbors, make([]string, 0), false, false,
-			nlayers, nfingers, w, rd, rs, ts)
-	}
-
-	var cka [nservers]*Clerk
-	for i := 0; i < nservers; i++ {
-		cka[i] = MakeClerk(kvh[i])
-	}
 
 }
 
@@ -537,16 +440,12 @@ func TestRealGetAndPut(t *testing.T) {
 	fmt.Printf("After put: value is %v\n", value)
 }
 
-// Test doesn't work, wrote it for subclassing sybil nodes
-/*
-func TestRealGetAndPutWithSybils(t *testing.T) {
+func TestEndToEnd(t *testing.T) {
 	runtime.GOMAXPROCS(4)
 
 	const nservers = 10
-	const nkeys = 20           // keys are strings from 0 to 99
+	const nkeys = 30           // keys are strings from 0 to 99
 	const k = nkeys / nservers // keys per node
-    const nsybilservers = 10
-    const sybilConnectionProb = 0.7
 
 	// parameters
 	constant := 5
@@ -561,18 +460,11 @@ func TestRealGetAndPutWithSybils(t *testing.T) {
 
 	var ws []*WhanauServer = make([]*WhanauServer, nservers)
 	var kvh []string = make([]string, nservers)
-    var ksvh []string = make([]string, nsybilservers)
-    var wss []*WhanauSybilServer = make([]*WhanauSybilServer, nsybilservers)
-    var sybilnormalneighbors [nsybilservers][]string
 	defer cleanup(ws)
 
 	for i := 0; i < nservers; i++ {
 		kvh[i] = port("basic", i)
 	}
-
-    for i := nservers; i < nsybilservers + nservers; i++ {
-        ksvh[i-nservers] = port("basic", i)
-    }
 
 	master_servers := []string{kvh[0], kvh[1], kvh[2]}
 
@@ -585,11 +477,23 @@ func TestRealGetAndPutWithSybils(t *testing.T) {
 			neighbors = append(neighbors, kvh[j])
 		}
 
-        rand.Seed(time.Now().UTC().UnixNano())
+		if i < 3 {
+			ws[i] = StartServer(kvh, i, kvh[i], neighbors, master_servers, true, false,
+				nlayers, nfingers, w, rd, rs, ts)
+		} else {
+			ws[i] = StartServer(kvh, i, kvh[i], neighbors, master_servers, false, false,
+				nlayers, nfingers, w, rd, rs, ts)
+		}
+	}
 
-        for k := 0; k < nsybilservers; k++ {
-            prob := rand.Float32()
-*/
+	var cka [nservers]*Clerk
+	for i := 0; i < nservers; i++ {
+		cka[i] = MakeClerk(kvh[i])
+	}
+
+	fmt.Printf("\033[95m%s\033[0m\n", "Test: End to End")
+
+}
 
 func TestPendingWrites(t *testing.T) {
 
